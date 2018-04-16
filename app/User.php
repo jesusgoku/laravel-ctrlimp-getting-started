a689c87490d06686b5b75e9b5e7cd18b9fe39219
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-04-16 01:29:49 -0300

    Added methods for OAuth user

--- app/User.php
+++ app/User.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use Notifiable;
 
     /**
